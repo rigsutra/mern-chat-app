f073ff13068effcba4e418571ebe8806a431e983
Ashish Singh <110900078+rigsutra@users.noreply.github.com>
2024-07-28 16:59:30 +0530

    Update index.js

--- mern-chat/backend/api/index.js
+++ mern-chat/backend/api/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-   origin: `'https://your-vercel-url.vercel.app'`,
+   origin: 'https://your-vercel-url.vercel.app',
   })
 );
 
